18bf74ac96eda3151a6b02a468f9c10b11089ec5
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2021-09-28 13:51:02 +0200

    update

---
+++ Source/SlapKnight_Legacy/Interfaces/I_Unit.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "I_Unit.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UI_Unit : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class SLAPKNIGHT_LEGACY_API II_Unit
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION()
+    virtual void MoveForward() = 0;
+	
+};
